7065847a63da9098caeab6f12bd2ae279d9f6e74
Kit Fenrir Amreik <94805218+Kitamreik@users.noreply.github.com>
2023-09-21 15:56:21 -0400

    Create edabit-python.py

---
+++ edabit-python.py
@@ -0,0 +1,9 @@
+#Create a function that takes two numbers as arguments and returns their sum.
+def addition(a, b):
+	return a + b 
+#Don't forget to return the result.
+#Ex. addition(3, 2) ➞ 5
+a = int(2)
+b = int(3)
+
+print(addition(a,b))
