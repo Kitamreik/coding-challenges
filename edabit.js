3d83a6db6d56a948a905ce5cae02e80ed4b4d740
Kit Fenrir Amreik <94805218+Kitamreik@users.noreply.github.com>
2023-09-22 22:41:24 -0400

    9/22

--- edabit.js
+++ edabit.js
@@ -16,3 +16,34 @@ addUp(4)
 
 //Video reference: https://www.youtube.com/watch?v=KEEKn7Me-ms
 //Docs: https://javascript.info/recursion#sum-all-numbers-till-the-given-one
+//9-21 above
+//------
+//9-22 below
+//Attempt again here: https://edabit.com/challenge/Gpy2qSFnfhGJnWMMj
+
+//Create a function that returns true 
+//if the first array can be nested inside the second and false otherwise.
+const arr1 = [1, 2, 3, 4]
+const arr2 = [0, 6]
+
+//Refactoring
+/*
+function canNest(arr1, arr2) {
+	if (Math.min(...arr1) < Math.min(...arr2)) {
+		//const rejection = (currently) => currently < Math.min(...arr1) < Math.min(...arr2)
+		//console.log(arr1.every(rejection))
+		//const impossible = (future) => future > Math.max(...arr1) > Math.max(...arr2) 
+		//console.log(arr1.every(impossible))
+		//return false
+	} else if (Math.max(...arr1) > Math.max(...arr2) ) {
+		//return false
+	} else {
+		//Math.min(...arr1) > Math.min(...arr2) //arr1's min is greater than arr2's min.
+		//Math.max(...arr1) < Math.max(...arr2) //arr1's max is less than arr2's max.
+		//return true
+	}
+}
+*/
+//canNest([1, 2, 3, 4], [0, 6]) //➞ true
+//canNest([9, 9, 8], [8, 9]) //➞ false, Note the strict inequality.
+//canNest([1, 2, 3, 4], [2, 3]) //➞ false
