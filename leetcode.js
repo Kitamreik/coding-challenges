1f9570d3359b8de47af7e467bc73052702e2a9b6
Kit Fenrir Amreik <94805218+Kitamreik@users.noreply.github.com>
2023-10-18 18:31:21 -0400

    Update leetcode.js
    Binary search problem

--- leetcode.js
+++ leetcode.js
@@ -38,3 +38,59 @@ var sortArray = function(nums) {
     }
 };
 */
+//-----
+//https://leetcode.com/problems/binary-search/
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    while (left < right) { //removing the equals sign
+      let mid = left + Math.floor((right-left+1)/2); //add math.floor and 
+      //add 1 to the r-l equation for right/upper mid
+      /* 
+       if (nums[mid] == target) {
+        return true //return the index of the found number
+      }
+      */
+     
+      if (target < nums[mid]) {
+        right = mid - 1
+      } else {
+        left = mid //make the left side become the middle
+        //console.log("return the index of the found number - left")
+        //console.log(nums[4]) //returns the target number
+        
+      }
+    }
+    //return something here to exit
+    //return -1 //works for one test case, 2 does not exist in nums so return -1
+    return nums[left] == target ? left : -1
+    //return the number array based on the left number and based on equal to the target, if it is, then say the left number if not return -1 
+};
+
+//Psuedocode
+/*
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right) {
+      let mid = left + ((right-left)/2);
+      if (nums[mid] == target) {
+        return true //return the index of the found number
+      } else if (target < nums[mid]) {
+        right = mid - 1
+      } else {
+        left = mid + 1
+        //console.log("return the index of the found number - left")
+        //console.log(nums[4]) //returns the target number
+        
+      }
+    }
+    //return something here to exit
+    return -1 //works for one test case, 2 does not exist in nums so return -1
+};
+*/
