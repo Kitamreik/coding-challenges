b430f07cb1da5865e0fcba7586040876f949fd81
Kit Fenrir Amreik <94805218+Kitamreik@users.noreply.github.com>
2024-07-31 20:59:44 -0400

    Update leetcode.js

--- leetcode.js
+++ leetcode.js
@@ -564,3 +564,24 @@ var canPlaceFlowers = function(flowerbed, n) {
         console.log(err)
     }
 };
+//------
+https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countOdds = function(low, high) {
+    let count = 0 ;
+
+    for(let i = low; i <= high, i++){
+        if(i % 2 !== 0){
+            count++;
+        }
+    }
+
+    return count;
+};
+
+countOdds(3, 7)
+// countOdds(8, 10)
